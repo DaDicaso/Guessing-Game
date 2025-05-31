5c83c3e1c243b038cc2bfc8d551ad5fab1fcd87f
Daksh <gargdaksh0616@gmail.com>
2025-05-31 16:53:18 +0530

    guessing and checking functionality by use of std::cmp::Ordering and converting the guess input to u32 type so that same data types are compared

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-
+use std::cmp::Ordering;
 use std::io;
 
 use rand::Rng;
@@ -7,13 +7,20 @@ fn main() {
     println!("-----Guess The Number------");
     
     let secret_number = rand::thread_rng().gen_range(1..=100);
-   
-    println!("The secret number is: {secret_number}");
+
     println!("Please input your guess.");
     let mut guess = String::new();
     io::stdin()
         .read_line(&mut guess).expect("Failed to read line");
     
     println!("You guessed: {guess}");
-    println!("The secret number is: {secret_number}");
+    // converting guess(string) to guess(i32 or u32) type by trimming and parsing 
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+    
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
+    println!("The secrete number is:  {secret_number}");
 }
