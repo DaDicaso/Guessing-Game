2ef28a44efdd44affa89cee40ed5dfdb41f6daa7
Daksh <gargdaksh0616@gmail.com>
2025-05-31 16:11:12 +0530

    Random number genration by use of rand::Rng

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
+
 use std::io;
+
 use rand::Rng;
 
 fn main() {
     println!("-----Guess The Number------");
     
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("Please input your guess: ");
-
-    //let apple = "Hello From apple"; // immutable
-    let mut guess = String::new(); // mutable
-
+   
+    println!("The secret number is: {secret_number}");
+    println!("Please input your guess.");
+    let mut guess = String::new();
     io::stdin()
         .read_line(&mut guess).expect("Failed to read line");
     
-    println!("Your guess and update {}", guess);
-
-
+    println!("You guessed: {guess}");
+    println!("The secret number is: {secret_number}");
 }
